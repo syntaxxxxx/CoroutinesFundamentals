68431ab8b67222aef0c0e5bd7b2844202dafa562
syntaxxxxx <fiqrihafzainislami@gmail.com>
2020-08-29 17:20:03 +0700

    implement use runnable

--- app/src/main/java/com/langitpayment/kotlincoroutinesfundamentals/MainActivity.kt
+++ app/src/main/java/com/langitpayment/kotlincoroutinesfundamentals/MainActivity.kt
@@ -3,13 +3,8 @@ package com.langitpayment.kotlincoroutinesfundamentals
 import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 import java.net.HttpURLConnection
 import java.net.URL
 
@@ -18,11 +13,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.d("Thread", Thread.currentThread().name)
+        Log.d("HAI", Thread.currentThread().name)
 
-        GlobalScope.launch(context = Dispatchers.IO) {
-            Log.d("Thread", Thread.currentThread().name)
-
+        /**
+         * call into another thread
+         * see for details https://developer.android.com/training/multiple-threads/define-runnable
+         * */
+        Thread(Runnable {
+            Log.d("HAI", Thread.currentThread().name)
             val imageUrl = URL("https://avatars2.githubusercontent.com/u/51395880?s=280&v=4")
             val connection = imageUrl.openConnection() as HttpURLConnection
             connection.doInput = true
@@ -31,10 +29,13 @@ class MainActivity : AppCompatActivity() {
             val inputStream = connection.inputStream
             val bitmap = BitmapFactory.decodeStream(inputStream)
 
-            launch(Dispatchers.Main) {
-                Log.d("Thread", Thread.currentThread().name)
+            /**
+             * post to the main thread use
+             * */
+            runOnUiThread{
+                Log.d("HAI", Thread.currentThread().name)
                 image.setImageBitmap(bitmap)
             }
-        }
+        }).start()
     }
 }
